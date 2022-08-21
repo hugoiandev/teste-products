d97b830675386befdb7d56b01f4cea796da2ddfb
hugo.silva <hugo.silva@accurate.com.br>
2022-08-21 04:10:21 -0300

    created filter of products

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Products from './pages/Products';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Products from "./pages/Products";
 
 function App() {
   return (
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -7,21 +7,34 @@ interface ICategory {
 }
 
 const Products = () => {
-
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [categories, setCategories] = useState<ICategory[]>([]);
 
-
   const getProducts = async () => {
-    const response = await fetch('productsCategory.json');
+    const response = await fetch("productsCategory.json");
 
     const json = await response.json();
     console.log(json.data.nodes);
 
     setProducts(json.data.nodes);
+    setFilteredProducts(json.data.nodes);
 
+    const filteredIds: string[] = [];
+    const filteredCategories: ICategory[] = [];
 
-  }
+    json.data.nodes.forEach((item: any) => {
+      const exist = filteredIds.includes(item.category.name);
+
+      if (!exist) {
+        filteredIds.push(item.category.name);
+
+        filteredCategories.push(item.category);
+      }
+    });
+
+    setCategories(filteredCategories);
+  };
 
   useEffect(() => {
     getProducts();
@@ -29,27 +42,50 @@ const Products = () => {
 
   return (
     <>
-    <header>
-      <form>
-        <select>
-          {<option value=""></option>}
-        </select>
-      </form>
-    </header>
-    <div>
-      <ul className={styles.containerItens}>
-      {products.map((item: any) => {
-        return (
-          <li className={styles.listItem} key={item.id}>
-            <p className={styles.title}>{item.name}</p>
-            <img className={styles.photo} src={item.images[0].asset.url} alt={item.images[0].alt} />
-          </li>
-        )
-      })}
-      </ul>
-    </div>
+      <header>
+        <form>
+          <select
+            onChange={(event) => {
+              if (event.target.value === "Todos") {
+                setFilteredProducts(products);
+              } else {
+                setFilteredProducts(
+                  products.filter(
+                    (item: any) => item.category._id === event.target.value
+                  )
+                );
+              }
+            }}
+          >
+            <option value="Todos">Todos</option>
+            {categories.map((category) => {
+              return (
+                <option key={category._id} value={category._id}>
+                  {category.name}
+                </option>
+              );
+            })}
+          </select>
+        </form>
+      </header>
+      <div>
+        <ul className={styles.containerItens}>
+          {filteredProducts.map((item: any) => {
+            return (
+              <li className={styles.listItem} key={item.id}>
+                <p className={styles.title}>{item.name}</p>
+                <img
+                  className={styles.photo}
+                  src={item.images[0].asset.url}
+                  alt={item.images[0].alt}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Products;
